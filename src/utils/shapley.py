671acdb6f2fd58addbb6d64cd0ca95be331abbb4
André-Ignace Ghonda Lukoki <contact@andreghonda.com>
2026-02-23 15:13:46 +0100

    adding utilities and testing pettingzoo

--- src/utils/shapley.py
+++ src/utils/shapley.py
@@ -16,3 +16,21 @@ def Shapley(N : list, v : dict) -> dict:
                 Shapley[i] += w * (v[S | {i}] - v[S])
     
     return dict(Shapley)
+
+""" Example
+
+N = [1, 2, 3]
+v = {
+    frozenset(): 0,
+    frozenset([1]): 0,
+    frozenset([2]): 0,
+    frozenset([3]): 0,
+    frozenset([1, 2]): 18,
+    frozenset([1, 3]): 14,
+    frozenset([2, 3]): 4,
+    frozenset([1, 2, 3]): 30
+}
+
+print(Shapley(N, v))
+> {1: 14.0, 2: 9.0, 3: 7.0}
+"""
